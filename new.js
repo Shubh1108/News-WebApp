84f88b2aee0287d16a4847f466fe0562136f3beb
Shubh1108 <shubh202024@gmail.com>
2024-11-17 23:32:58 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,5 +1,5 @@
 const API_KEY = "70ffc9f537aa4b3e93cc82e32b3508be";
-const url = "https://newsapi.org/v2/everything?q=";
+const url = "http://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load', () => fetchNews("India"));
 
